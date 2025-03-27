f8c78fe55dc237254c8a99fe973ecef3a3ff44b3
ItaloVieira06 <italoespiritosantovieira@gmail.com>
2025-03-27 16:00:25 -0300

    methods adition

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res)=>{
-    res.end("Hello World")
+    const {method, url} = req
+
+    if (method == 'GET' && url == '/users'){
+        return res.end('Listagem de usuários')
+    }
+
+    if(method == 'POST' && url == '/users'){
+        return res.end('Criação de Usuários')
+    }
 })
 
 server.listen(3333)
