635030d680bc80d0ed4cea36780d46a091199f5a
ItaloVieira06 <italoespiritosantovieira@gmail.com>
2025-03-27 18:40:45 -0300

    streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,19 @@
+import { Readable } from 'node:stream'
+
+class oneToHundredStream extends Readable{
+    index = 1
+    _read(){
+      const i = this.index++
+
+      if (i > 100){
+        this.push(null)
+      } else{
+        const buf = Buffer.from(string(i))
+
+        this.push(buf)
+      }
+    }
+}
+
+new oneToHundredStream()
+ .pipe(process.stdout)
