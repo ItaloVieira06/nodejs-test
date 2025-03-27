46d4724d307a4d493e6bd96d167eb87d4bee1126
ItaloVieira06 <italoespiritosantovieira@gmail.com>
2025-03-27 16:58:15 -0300

    added users internally

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 import http from 'node:http'
 
+const user = []
+
 const server = http.createServer((req, res)=>{
     const {method, url} = req
 
     if (method == 'GET' && url == '/users'){
-        return res.end('Listagem de usuários')
+        return res
+        .setHeader('Content-type', 'applicaion/json')
+        .end(JSON.stringify(users))
     }
 
     if(method == 'POST' && url == '/users'){
+        user.push({
+            id: 1,
+            name: 'Jhon Doe',
+            email: 'jhondoe@example.com',
+        })
         return res.end('Criação de Usuários')
     }
 })
