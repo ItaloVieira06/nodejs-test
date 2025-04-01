2c182b2c8f6264453e7d5bbd6ab14d43cdc744c3
Ítalo do Espírito Santo Vieira <italoespiritosantovieira@gmail.com>
2025-04-01 18:52:13 -0300

    delete commit2

--- src/routes.js
+++ src/routes.js
@@ -35,7 +35,11 @@ export const routes = [
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
         handlers: (req, res) => {
-            return res.end()
+            const {id} = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         },
     }
 
