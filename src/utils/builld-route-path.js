f71aa16a12505fabf428f3ac263685cc459dec74
Ítalo do Espírito Santo Vieira <italoespiritosantovieira@gmail.com>
2025-04-02 17:34:57 -0300

    query requested added

--- src/utils/builld-route-path.js
+++ src/utils/builld-route-path.js
@@ -2,7 +2,7 @@ export function buildRoutePath(path){
     const routeParameterRegex = /:((a-zA-z)+)/g
     const pathWithParams = path.replaceAll(routeParametersRegex, '(?<id>[a-z0-9\-_]+)')
 
-    const pathRegex = new RegExp(`^${pathWithParams}`)
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>)?$`)
 
     return pathRegex
 }
